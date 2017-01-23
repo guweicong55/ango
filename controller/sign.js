ad8501eaa1a81579e6d516a15090470a6d35f9c5
qige <443156542@qq.com>
2017-01-23 13:46:11 +0800

    优化登录模块

--- controller/sign.js
+++ controller/sign.js
@@ -39,6 +39,10 @@ exports.signUp = function (req, res) {
 		ep.emit('reg_err', '用户名长度必须大于6位。');
 		return;
 	}
+	if (validator.isEmail(user_name)) {
+		ep.emit('reg_err', '用户名不能为邮箱。');
+		return;
+	}
 	if (!validator.isEmail(email)) {
 		ep.emit('reg_err', '请输入合法邮箱。');
 		return;
@@ -86,42 +90,28 @@ exports.signUp = function (req, res) {
 	});
 }
 
+// 登录模块账号邮箱均可以登录
 exports.signIn = function (req, res) {
 	var data = req.body;
 	var account = validator.trim(data.account).toLowerCase();
 	var password = validator.trim(data.password);
 
-	if (validator.isEmail(account)) {
-		user.find({
-			email: account,
-		}, function (err, doc) {
-			if (err) {
-				console.log('登录失败' + err);
-			} else if (doc.length === 0) {
-				res.send('用户不存在');
-			} else if (doc.length > 0) {
-				if (tool.mkCompare(password, doc[0].password)) {
-					res.send('登录成功');
-				} else {
-					res.send('密码错误');
-				};
+	user.find({'$or': [{
+		email: account
+	}, {
+		user_name: account
+	}]}, function (err, doc) {
+		if (err) {
+			console.log('登录失败' + err);
+		} else if (doc.length === 0) {
+			res.send('用户不存在');
+		} else if (doc.length > 0) {
+			if (tool.mkCompare(password, doc[0].password)) {
+				//req.session.user = account;
+				res.send('登录成功');
+			} else {
+				res.send('密码错误');
 			};
-		});
-	} else {
-		user.find({
-			user_name: account,
-		}, function (err, doc) {
-			if (err) {
-				console.log('登录失败' + err);
-			} else if (doc.length === 0) {
-				res.send('用户不存在');
-			} else if (doc.length > 0) {
-				if (tool.mkCompare(password, doc[0].password)) {
-					res.send('登录成功');
-				} else {
-					res.send('密码错误');
-				};
-			};
-		});
-	};
+		};
+	});
 }
