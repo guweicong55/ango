3fc25ba6004cb2e2af5891078309fc3590bdfd5b
qige <443156542@qq.com>
2017-02-10 17:47:41 +0800

    uirouter 嵌套

--- static/js/app.js
+++ static/js/app.js
@@ -2,31 +2,30 @@ var app = angular.module('app', ['ui.router']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/');
+	$urlRouterProvider.when('/', '/main-article-list');
 
-	$stateProvider.state({
-		name: 'main',
+	$stateProvider.state('main', {
 		url: '/',
 		templateUrl: './temp/main.html',
 		controller: 'main'
-	}).state({
-		name: 'signup',
+	}).state('signup', {
 		url: '/signup',
 		templateUrl: './temp/signup.html',
 		controller: 'signup'
-	}).state({
-		name: 'signin',
+	}).state('signin', {
 		url: '/signin',
 		templateUrl: './temp/signin.html',
 		controller: 'signin'
-	}).state({
-		name: 'publish',
+	}).state('publish', {
 		url: '/publish',
 		templateUrl: './temp/publish.html',
 		controller: 'publish'
-	}).state({
-		name: 'article',
+	}).state('article', {
 		url: '/article/:id',
 		templateUrl: './temp/article.html',
 		controller: 'article'
+	}).state('main.main-article-list', {
+		url: 'main-article-list',
+		templateUrl: './temp/main-article-list.html',
 	})
 })
